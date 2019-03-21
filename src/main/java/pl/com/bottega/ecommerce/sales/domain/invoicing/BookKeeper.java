1929bc32889c2d81c85591e74d880ac7de2530ed
Karolina Płóciennik <209434@edu.p.lodz.pl>
2019-03-21 17:38:49 +0100

    use InvoiceFactory in BookKeeper

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -22,7 +22,9 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 public class BookKeeper {
 
     public Invoice issuance(ClientData client, List<RequestItem> items) {
-        Invoice invoice = new Invoice(Id.generate(), client);
+
+        InvoiceFactory invoiceFactory = new InvoiceFactory();
+        Invoice invoice = invoiceFactory.createInvoiceInstance(client, "invoice");
 
         for (RequestItem item : items) {
             Money net = item.getTotalCost();
