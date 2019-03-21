eeaeb673f47d2fc17630acda5c9d4a699560d25b
Jakub Mroczek <jakub.mroczek@tomtom.com>
2019-03-21 16:54:36 +0100

    injected InvoiceFactory to the BookeKeeper

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceFactory.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceFactory.java
@@ -1,5 +1,10 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+
 public interface InvoiceFactory {
+
+    public Invoice create(Id invoiceId, ClientData client);
 
 }
