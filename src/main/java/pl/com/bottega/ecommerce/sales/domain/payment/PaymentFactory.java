bfde3a8a52825e17f7319228c7847f838dae0b34
jusmis <44554496+jusmis@users.noreply.github.com>
2019-03-21 17:07:18 +0100

    --Utworzenie fabryk dla klas Invoice i Payment

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/payment/PaymentFactory.java
@@ -0,0 +1,12 @@
+package pl.com.bottega.ecommerce.sales.domain.payment;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public class PaymentFactory {
+
+        public static Payment create(ClientData clientData, Money amount) {
+            return new Payment(Id.generate(), clientData, amount.multiplyBy(-1));
+        }
+}
