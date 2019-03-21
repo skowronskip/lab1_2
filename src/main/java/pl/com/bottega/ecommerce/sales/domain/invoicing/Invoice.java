c09afb5fdf6d7197d6d764ecef8f9e8460eeaca7
Piotr Domagała <piotr.domagala@ragnarson.com>
2019-03-21 17:19:40 +0100

    Refactor - Refactored Invoice class

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/Invoice.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/Invoice.java
@@ -48,6 +48,10 @@ public class Invoice {
         gros = gros.add(item.getGros());
     }
 
+    public static Invoice createObject(Id invoiceId, ClientData client) {
+        return new Invoice(invoiceId, client);
+    }
+
     /**
      *
      * @return immutable projection
