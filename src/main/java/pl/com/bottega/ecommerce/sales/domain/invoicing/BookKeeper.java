c65f308737d22312b619b5d75f33e4d2cc456642
Hubert Liberadzki <209377@edu.p.lodz.pl>
2019-03-21 17:49:00 +0100

    deleted unused imports

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -12,9 +12,7 @@
  */
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
-import java.util.List;
 
-import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
