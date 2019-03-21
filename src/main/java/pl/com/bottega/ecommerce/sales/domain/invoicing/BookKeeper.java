18c8b97b480dbed660565c1e72248546ab569219
210952 Skowronski Pawel <210952@edu.p.lodz.pl>
2019-03-21 16:55:25 +0100

    Creation of InvoiceFactory

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -22,7 +22,8 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 public class BookKeeper {
 
     public Invoice issuance(ClientData client, List<RequestItem> items) {
-        Invoice invoice = new Invoice(Id.generate(), client);
+        InvoiceFactory invoiceFactory = new InvoiceFactory();
+        Invoice invoice = invoiceFactory.createInvoice(client);
 
         for (RequestItem item : items) {
             Money net = item.getTotalCost();
