7725473100ee9238676a9adb9db0f433f5e5318e
rogo6 <piotrrogozinski95@gmail.com>
2019-03-21 17:23:52 +0100

    Create InvoiceFactory, refactored issuance method

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -20,39 +20,13 @@ import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeper {
+	
+    public Invoice issuance(InvoiceRequest invoiceRequest, TaxCalculator taxCalculator, InvoiceFactory invoiceFactory) {
+        Invoice invoice = invoiceFactory.createInvoice(Id.generate(), invoiceRequest.getClient());
 
-    public Invoice issuance(ClientData client, List<RequestItem> items) {
-        Invoice invoice = new Invoice(Id.generate(), client);
-
-        for (RequestItem item : items) {
-            Money net = item.getTotalCost();
-            BigDecimal ratio = null;
-            String desc = null;
-
-            switch (item.getProductData()
-                        .getType()) {
-                case FOOD:
-                    ratio = BigDecimal.valueOf(0.07);
-                    desc = "7% (F)";
-                    break;
-                case STANDARD:
-                    ratio = BigDecimal.valueOf(0.23);
-                    desc = "23%";
-                    break;
-                case DRUG:
-                    ratio = BigDecimal.valueOf(0.05);
-                    desc = "5% (D)";
-                    break;
-                default:
-                    throw new IllegalArgumentException(item.getProductData()
-                                                           .getType()
-                                                       + " not handled");
-            }
-
-            Money taxValue = net.multiplyBy(ratio);
-
-            Tax tax = new Tax(taxValue, desc);
-
+        for (RequestItem item : invoiceRequest.getItems()) {
+        	Tax tax = taxCalculator.calculateTax(item);
+        	Money net = item.getTotalCost();
             InvoiceLine invoiceLine = new InvoiceLine(item.getProductData(), item.getQuantity(), net, tax);
             invoice.addItem(invoiceLine);
         }
