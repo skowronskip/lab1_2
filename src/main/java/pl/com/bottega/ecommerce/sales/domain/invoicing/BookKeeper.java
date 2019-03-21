3779a39fc4c017f3b001849d3e6a82340af4030d
Kalinowski.Mikołaj <209347@edu.p.lodz.pl>
2019-03-21 18:14:12 +0100

    Usunięcie zakomentowanego kodu.

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -26,28 +26,6 @@ public class BookKeeper {
 
         for (RequestItem item : invoiceRequest.getItems()) {
             Money net = item.getTotalCost();
-//            BigDecimal ratio = null;
-//            String desc = null;
-
-//            switch (item.getProductData().getType()) {
-//                case FOOD:
-//                    ratio = BigDecimal.valueOf(0.07);
-//                    desc = "7% (F)";
-//                    break;
-//                case STANDARD:
-//                    ratio = BigDecimal.valueOf(0.23);
-//                    desc = "23%";
-//                    break;
-//                case DRUG:
-//                    ratio = BigDecimal.valueOf(0.05);
-//                    desc = "5% (D)";
-//                    break;
-//                default:
-//                    throw new IllegalArgumentException(item.getProductData().getType() + " not handled");
-//            }
-
-//            Money taxValue = net.multiplyBy(ratio);
-
             Tax tax = taxCalculator.calculateTax(item);
 
             InvoiceLine invoiceLine = new InvoiceLine(item.getProductData(), item.getQuantity(), net, tax);
