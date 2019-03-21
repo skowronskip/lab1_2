b2a3051737add0794c8a22be6da9b6f1eaff27f9
jusmis <44554496+jusmis@users.noreply.github.com>
2019-03-21 17:28:40 +0100

    --Utworzenie interfejsu ITaxPolicy

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/ITaxPolicy.java
@@ -0,0 +1,9 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public interface ITaxPolicy {
+
+    public Tax calculateTax(ProductData productData, Money net);
+}
