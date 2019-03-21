27f40ca460515312b223505789f5463141679aac
210952 Skowronski Pawel <210952@edu.p.lodz.pl>
2019-03-21 17:02:43 +0100

    Use InvoiceRequest as initilize parameter for BookKeeper

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -21,11 +21,11 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeper {
 
-    public Invoice issuance(ClientData client, List<RequestItem> items) {
+    public Invoice issuance(InvoiceRequest invoiceRequest) {
         InvoiceFactory invoiceFactory = new InvoiceFactory();
-        Invoice invoice = invoiceFactory.createInvoice(client);
+        Invoice invoice = invoiceFactory.createInvoice(invoiceRequest.getClient());
 
-        for (RequestItem item : items) {
+        for (RequestItem item : invoiceRequest.getItems()) {
             Money net = item.getTotalCost();
             BigDecimal ratio = null;
             String desc = null;
