b7f01687a21500bb913505596e4353dd7ed71ee1
Michał Grzelak <grzelakm@outlook.com>
2019-03-22 20:30:23 +0100

    Added DefaultTaxCalculator class (not implemented yet); Made issuance method use TaxCalculator interface

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/DefaultTaxCalculator.java
@@ -0,0 +1,9 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+public class DefaultTaxCalculator implements TaxCalculator {
+
+    @Override
+    public Tax calculate(RequestItem item) {
+        return null;
+    }
+}
