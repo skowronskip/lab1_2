677a01b0bb54dba68c2226ae6dd092e8a619c246
Michał Grzelak <grzelakm@outlook.com>
2019-03-22 19:55:54 +0100

    Added PaymentFactory

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/payment/PaymentFactory.java
@@ -0,0 +1,11 @@
+package pl.com.bottega.ecommerce.sales.domain.payment;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public class PaymentFactory {
+    Payment create(Id aggregateId, ClientData clientData, Money amount) {
+        return new Payment(aggregateId, clientData, amount);
+    }
+}
