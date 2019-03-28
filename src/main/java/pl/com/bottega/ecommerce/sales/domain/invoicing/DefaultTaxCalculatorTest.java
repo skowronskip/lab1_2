0e30f90c51ba9738d47878c0553d8d91268a62b0
adamsylla97 <209474@edu.p.lodz.pl>
2019-03-28 17:05:26 +0100

    *added test for DefaultTaxCalculator for drugs

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/DefaultTaxCalculatorTest.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/DefaultTaxCalculatorTest.java
@@ -1,14 +1,38 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.domain.invoicing.RequestItem;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class DefaultTaxCalculatorTest {
 
     @Test
-    public void calculateTest(){
+    public void calculateDrugTest(){
+
+        ProductData productData = new ProductData();
+
+        Money money = new Money(100,"EUR");
+        Money moneyForTax = new Money(5,"EUR");
 
 
+        productData.setName("tempProduct");
+        productData.setPrice(money);
+        productData.setType(ProductType.DRUG);
+        productData.setProductId(Id.generate());
+
+        RequestItem requestItem = new RequestItem(productData,1,money);
+
+        Tax tax = Tax.createTax(moneyForTax, "vat");
+
+        DefaultTaxCalculator calculator = new DefaultTaxCalculator();
+
+        Tax resultTax = calculator.calculate(requestItem);
+
+        Assertions.assertEquals(tax.getAmount(),resultTax.getAmount());
 
     }
 
