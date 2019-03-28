e2502625d0371e3f4fc0a584d888da1795260887
Michał Grzelak <grzelakm@outlook.com>
2019-03-28 17:15:35 +0100

    Added empty MoneyTest

---
+++ src/test/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -0,0 +1,32 @@
+package pl.com.bottega.ecommerce.sharedkernel;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MoneyTest {
+
+    @Test
+    public void multiplyBy() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void subtract() {
+    }
+
+    @Test
+    public void greaterThan() {
+    }
+
+    @Test
+    public void lessThan() {
+    }
+
+    @Test
+    public void lessOrEquals() {
+    }
+}
