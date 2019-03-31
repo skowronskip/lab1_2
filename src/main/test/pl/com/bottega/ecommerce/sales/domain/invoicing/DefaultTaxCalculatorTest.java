7c51783a58a50554f18a144504df0bbf7d278a83
209347 <209347@edu.p.lodz.pl>
2019-03-31 21:47:15 +0200

    lab2_2 10 - Added test for Tax calculator - Drug

--- src/main/test/pl/com/bottega/ecommerce/sales/domain/invoicing/DefaultTaxCalculatorTest.java
+++ src/main/test/pl/com/bottega/ecommerce/sales/domain/invoicing/DefaultTaxCalculatorTest.java
@@ -18,7 +18,8 @@ import static org.junit.Assert.*;
 
 public class DefaultTaxCalculatorTest {
 
-    private final Money price = new Money(new BigDecimal(10), Currency.getInstance("EUR"));;
+    private final Money price = new Money(new BigDecimal(10), Currency.getInstance("EUR"));
+
     private final DefaultTaxCalculator defaultTaxCalculator = new DefaultTaxCalculator();
 
     @Test
@@ -40,6 +41,18 @@ public class DefaultTaxCalculatorTest {
 
         Tax calculatedTax = defaultTaxCalculator.calculateTax(requestItem);
         Tax expectedTax = new Tax(new Money(23), "23%");
+
+        Assert.assertThat(calculatedTax.getAmount(), Matchers.equalTo(expectedTax.getAmount()));
+        Assert.assertThat(calculatedTax.getDescription(), Matchers.equalTo(expectedTax.getDescription()));
+    }
+
+    @Test
+    public void calculateTaxForDrug() {
+        ProductData testProduct = new ProductData(Id.generate(), price, "testProduct", ProductType.DRUG, Date.from(Instant.now()));
+        RequestItem requestItem = new RequestItem(testProduct, 10, price.multiplyBy(10));
+
+        Tax calculatedTax = defaultTaxCalculator.calculateTax(requestItem);
+        Tax expectedTax = new Tax(new Money(5), "5% (D)");
 
         Assert.assertThat(calculatedTax.getAmount(), Matchers.equalTo(expectedTax.getAmount()));
         Assert.assertThat(calculatedTax.getDescription(), Matchers.equalTo(expectedTax.getDescription()));
