a6870fba9ad0a407d9918da9139a93f9ef3a2c1d
209347 <209347@edu.p.lodz.pl>
2019-03-31 21:45:05 +0200

    lab2_2 9 - Added test for Tax calculator - Standart, Deleted code duplicates and changed tested values

--- src/main/test/pl/com/bottega/ecommerce/sales/domain/invoicing/DefaultTaxCalculatorTest.java
+++ src/main/test/pl/com/bottega/ecommerce/sales/domain/invoicing/DefaultTaxCalculatorTest.java
@@ -2,6 +2,7 @@ package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
 import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
@@ -17,15 +18,29 @@ import static org.junit.Assert.*;
 
 public class DefaultTaxCalculatorTest {
 
+    private final Money price = new Money(new BigDecimal(10), Currency.getInstance("EUR"));;
+    private final DefaultTaxCalculator defaultTaxCalculator = new DefaultTaxCalculator();
+
     @Test
     public void calculateTaxForFood() {
-        Money price = new Money(new BigDecimal(10), Currency.getInstance("EUR"));
         ProductData testProduct = new ProductData(Id.generate(), price, "testProduct", ProductType.FOOD, Date.from(Instant.now()));
-        RequestItem requestItem = new RequestItem(testProduct, 5, price.multiplyBy(5));
+        RequestItem requestItem = new RequestItem(testProduct, 10, price.multiplyBy(10));
 
-        DefaultTaxCalculator defaultTaxCalculator = new DefaultTaxCalculator();
         Tax calculatedTax = defaultTaxCalculator.calculateTax(requestItem);
-        Tax expectedTax = new Tax(new Money(3.5), "7% (F)");
+        Tax expectedTax = new Tax(new Money(7), "7% (F)");
+
+        Assert.assertThat(calculatedTax.getAmount(), Matchers.equalTo(expectedTax.getAmount()));
+        Assert.assertThat(calculatedTax.getDescription(), Matchers.equalTo(expectedTax.getDescription()));
+    }
+
+    @Test
+    public void calculateTaxForStandard() {
+        ProductData testProduct = new ProductData(Id.generate(), price, "testProduct", ProductType.STANDARD, Date.from(Instant.now()));
+        RequestItem requestItem = new RequestItem(testProduct, 10, price.multiplyBy(10));
+
+        Tax calculatedTax = defaultTaxCalculator.calculateTax(requestItem);
+        Tax expectedTax = new Tax(new Money(23), "23%");
+
         Assert.assertThat(calculatedTax.getAmount(), Matchers.equalTo(expectedTax.getAmount()));
         Assert.assertThat(calculatedTax.getDescription(), Matchers.equalTo(expectedTax.getDescription()));
     }
