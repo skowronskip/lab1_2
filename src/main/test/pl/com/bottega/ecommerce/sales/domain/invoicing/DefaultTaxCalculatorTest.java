0e06590d893bdf589544785a2f107c0cf6106d29
Kaczmarczyk.Michal <209344@edu.p.lodz.pl>
2019-03-31 23:10:42 +0200

    added testDefaultTaxCalculatorForFoodProductType test

--- src/main/test/pl/com/bottega/ecommerce/sales/domain/invoicing/DefaultTaxCalculatorTest.java
+++ src/main/test/pl/com/bottega/ecommerce/sales/domain/invoicing/DefaultTaxCalculatorTest.java
@@ -22,4 +22,16 @@ public class DefaultTaxCalculatorTest {
         Tax tax = taxCalculator.calculate(requestItem);
         Assert.assertThat(tax.getAmount(), is(taxMoneyExpected));
     }
+
+    @Test
+    public void testDefaultTaxCalculatorForFoodProductType() {
+        TaxCalculator taxCalculator = new DefaultTaxCalculator();
+        Money money = new Money(1);
+        Money taxMoneyExpected = new Money(0.07);
+        ProductData productData = new ProductData(new Id("id"), money, "product", ProductType.FOOD, null);
+        RequestItem requestItem = new RequestItem(productData, 1, money);
+
+        Tax tax = taxCalculator.calculate(requestItem);
+        Assert.assertThat(tax.getAmount(), is(taxMoneyExpected));
+    }
 }
