376599ac6cfb9ca5596175a170c498c5df6acd7d
210952 Skowronski Pawel <210952@edu.p.lodz.pl>
2019-04-11 13:14:06 +0200

    Lab 2_2 test if invoice calculate proper net and gros value with added item

--- src/test/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceTest.java
+++ src/test/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceTest.java
@@ -3,6 +3,7 @@ package pl.com.bottega.ecommerce.sales.domain.invoicing;
 import org.junit.Test;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,5 +16,17 @@ public class InvoiceTest {
 
         assertEquals(new Money(0), invoice.getNet());
         assertEquals(new Money(0), invoice.getGros());
+    }
+
+    @Test
+    public void shouldHaveProperNetAndGrosValuesWithItemAdded() {
+        InvoiceFactory invoiceFactory = new InvoiceFactory();
+        Invoice invoice = invoiceFactory.createInvoice(new ClientData(Id.generate(), "name"));
+        InvoiceLine item = new InvoiceLine(null, 1, new Money(100), new Tax(new Money(20), "desc"));
+        invoice.addItem(item);
+
+        assertEquals(new Money(100), invoice.getNet());
+        assertEquals(new Money(120), invoice.getGros());
+
     }
 }
